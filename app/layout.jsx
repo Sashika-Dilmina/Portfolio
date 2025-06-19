d276df6114131e9a409382e55c938a907f458e01
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-06-19 15:27:41 +0530

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
 import {JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono= JetBrains_Mono({
+
+const jetbrainsMono= JetBrains_Mono({ 
   subsets:["latin"],
   weights:["100","200","300","400","500","600","700","800"],
   variable: "--font-jetbrainsMono",
@@ -18,8 +21,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header/>
-
-        {children}</body>
+        <StairTransition/>
+        <PageTransition>{children}</PageTransition>
+        </body>
     </html>
   );
 }
---
+++ components/StairTransition.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+
+
+const StairTransition = () => {
+    return <AnimatePresence mode="wait">StairTransition</AnimatePresence>;
+    
+};
+
+export default StairTransition;
