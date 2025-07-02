2681c11759ef52ee1e03da7ce9fbe7cbbe7e5914
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-07-03 00:54:12 +0530

    new

--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,46 @@
 import { Button } from "@/components/ui/button";
-import {FiDownload} from "react-icons/fi";
-
+import { FiDownload } from "react-icons/fi";
 
 // components
-import Social from "@/components/Social";
-
+import Socials from "@/components/Social"; 
 
 const Home = () => {
-  return(
+  return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between 
-         xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br/> <span className="text-accent">Sashika Dilmina</span>
-              </h1>
-              <p className="max-w-[500px] mb-9 text-white/80">
-                I specialize in crafting elegant digital experiences and have hands-on proficiency
-                 in a variety of programming languages and modern technologies as a Software Engineering undergraduate.
-              </p>
-              {/*btn and socials*/}
-              <div className="flex flex-col xl:flex-row items-center gap-8">
-                <Button 
-                  variant="outline" 
-                  size="lg"
-                  className="uppercase flex items-center gap-2"
-                  >
-                  <span>Download CV</span>
-                  <FiDownload className="text-xl" />
+              Hello I'm <br /> <span className="text-accent">Sashika Dilmina</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I specialize in crafting elegant digital experiences and have hands-on proficiency
+              in a variety of programming languages and modern technologies as a Software Engineering undergraduate.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social/>
+                <Socials />
               </div>
-              </div>
+            </div>
           </div>
+
           {/* photo */}
           <div>photo</div>
         </div>
       </div>
-      </section>
-
-  ) ;
-    
+    </section>
+  );
 };
 
 export default Home;
--- components/Social.jsx
+++ components/Social.jsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
 
-import{FaGithub, falinkedinIn, FaYoutube,FaTwitter} from 'react-icons/fa'
+import{FaGithub, FalinkedinIn, FaTwitter} from 'react-icons/fa';
 
-const Social = () =>{
+const socials =[
+    {icon: <FaGithub/>,path: ''},
+    {icon: <FalinkedinIn/>,path: ''},
+    {icon: <FaTwitter/>,path: ''},
+
+];
     
-        <div>Socials</div>
+const Social = ({contoinerStyles,iconStyles}) =>{
+    return <div className={contoinerStyles}>
+        {socials.map((item,index)=>{
+            return <Link key={index} href={item.path} className={iconStyles}>
+                {item.icon}
+            </Link>
+        })}
+        </div>;
     
 };
 
-export default Social;
+export default Social;  
