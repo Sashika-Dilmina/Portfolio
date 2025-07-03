f466fdae66d74a093ae13e3fc22bb4045dfcff76
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-07-03 17:02:13 +0530

    new

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+"use client";
+
+import {motion} from "framer-motion";
+import Image from "next/image";
+
+
+
 
 const Photo = () =>{
-    return <div>Photo</div>
+    return(
+         <div className="w-full h-full relative">
+        <motion.div 
+            initial={{opacity:0}} 
+            animate={{
+                opacity: 1,
+                transition: {delay: 2, duration: 0.4, ease:'easeIn'},
+                }}
+            >
+                
+            <div className="w-[350px] h-[350px] xl:w-[498px] xl:h-
+            [498px] mix-blend-lighten">
+                <Image
+                 src="/assets/photo.png" 
+                 priority 
+                 quality={100} 
+                 fill 
+                alt="" 
+                className="object-contain"
+                />
+            </div>
+        </motion.div>
+        </div>
     
+    );
 };
 
 export default Photo;
