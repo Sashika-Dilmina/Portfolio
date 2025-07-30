5bbd503256b780d10bdb077a1379da1737ea912e
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-07-31 01:22:28 +0530

    update

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Nav from "./nav";
+import { Button } from "@/components/ui/button";
+import MobileNav from "./MobileNav";
+
+
+//components
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold cursor-pointer">
+            Sashika Dilmina<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* dekstop nav and hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href="/contact">
+             <Button>Hire me</Button>
+            </Link>
+        </div>
+
+        {/* mobile nav*/}
+        <div className="xl:hidden">
+          <MobileNav />
+          </div> 
+      </div>
+    </header>
+  );
+};
+
+export default Header;
