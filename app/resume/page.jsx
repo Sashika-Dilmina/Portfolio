c6c9cd6272d1a0fdd336d38e6a2538db7b11b7c6
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-07-31 14:59:28 +0530

    update

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -102,6 +102,16 @@ const education = {
             Degree: "(BSc) Hons in Software Engineering",
             duration: "2023 - 2027",
         },
+         {
+            Campus: "CINEC Campus",
+            Degree: "(BSc) Hons in Software Engineering",
+            duration: "2023 - 2027",
+        },
+         {
+            Campus: "CINEC Campus",
+            Degree: "(BSc) Hons in Software Engineering",
+            duration: "2023 - 2027",
+        },
     ],
 };
 
@@ -219,7 +229,34 @@ const Resume = () => {
                     </TabsContent>
                     {/* education */}
                     <TabsContent value="education" className="w-full">
-                        education
+                        <div className="flex flex-col gap-[30px] text-center xl:text-left" >
+                            <h3 className="text-4xl font-bold">{education.title}</h3>
+                            <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
+                            <ScrollArea className="h-[400px]">
+                                <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                                    {education.items.map((item,index)=>{
+                                        return (
+                                            <li key={index} 
+                                            className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl 
+                                            flex flex-col justify-center items-center lg:items-start
+                                            gap-1"
+                                            >
+
+                                            <span className="text-accent">{item.duration}</span>
+                                            <h3 className="text-xl max-w-[260px] min-h-[60px]
+                                            text-center lg:text-left">{item.Degree}
+                                            </h3>
+                                            <div className="flex items-center gap-3">
+                                                {/*dot*/}
+                                                <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                                                <p className="text-white/60">{item.Campus}</p>
+                                            </div>
+                                        </li>
+                                        ) 
+                                    })}
+                                </ul>
+                            </ScrollArea>
+                        </div>
                     </TabsContent>
                     {/* skills */}
                     <TabsContent value="skills" className="w-full">
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
   		extend: {
 
 			colors: {
-			primary: "#030712",
+			primary: "#09090b",
 			accent: {
 				DEFAULT: "#00ff99",
 				hover: "#00e187",
