1b2e547c1470a731a06d31ec3ea7019f4a0b8b44
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-03-15 10:02:33 +0530

    cv update

--- app/page.jsx
+++ app/page.jsx
@@ -24,7 +24,7 @@ const Home = () => {
           </p>
           {/* btn and socials*/}
           <div className="flex flex-col xl:flex-row items-center gap-8">
-            <a href="/Sashika Dilmina-CV.pdf" download>
+            <a href="/SASHIKA_DILMINA_SOFTWARE_ENGINEERING_INTERN.pdf" download>
             <Button 
                 variant="outline" 
                 size="lg" 
